f7252a2c1b36387cd9ada3fdb39f316b18535293
apgiola <96624287+apgiola@users.noreply.github.com>
2022-10-09 20:29:02 -0400

    added periods to end of output and remove str type

--- highwaynumbers.py
+++ highwaynumbers.py
@@ -1,21 +1,21 @@
 #Take user input and check if valid
 highway_num = int(input())
 if highway_num < 1 or highway_num > 999 or highway_num % 100 == 0:
-    print('{} is not a valid interstate highway number'.format (highway_num))
+    print('{} is not a valid interstate highway number.'.format (highway_num))
     quit()
 #type of highway and what primary it services
 if highway_num > 100:
     road_type = 'auxiliary'
-    auxiliary = str(highway_num % 100)
+    auxiliary = highway_num % 100
 else:
     road_type = 'primary'
     auxiliary = ''
 
 #direction of service
 if highway_num % 2 == 0:
-    direction = 'east/west'
+    direction = 'east/west.'
 else:
-    direction = 'north/south'
+    direction = 'north/south.'
 
 #output create list
 output = ['I-{}'.format(highway_num), 'is', road_type + ',']
