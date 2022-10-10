4b209b4374c9f2dc2a7a3686d902bd06ffc1f21e
apgiola <96624287+apgiola@users.noreply.github.com>
2022-10-09 20:27:12 -0400

    additional validation on input

--- highwaynumbers.py
+++ highwaynumbers.py
@@ -1,25 +1,27 @@
 #Take user input and check if valid
 highway_num = int(input())
-if highway_num < 1 or highway_num > 999:
-    print('{} is not a valid highway number'.format (highway_num))
+if highway_num < 1 or highway_num > 999 or highway_num % 100 == 0:
+    print('{} is not a valid interstate highway number'.format (highway_num))
     quit()
 #type of highway and what primary it services
 if highway_num > 100:
     road_type = 'auxiliary'
-    serving = str(highway_num % 100)
+    auxiliary = str(highway_num % 100)
 else:
     road_type = 'primary'
-    serving = ''
+    auxiliary = ''
 
 #direction of service
 if highway_num % 2 == 0:
-    going = 'east/west'
+    direction = 'east/west'
 else:
-    going = 'north/south'
+    direction = 'north/south'
 
 #output create list
 output = ['I-{}'.format(highway_num), 'is', road_type + ',']
-if serving:
-    output.extend(['serving', 'I-{},'.format(serving)])
-output.extend(['going', going])
+#create output branch to add to output variable if it is an auxiliary highway.
+if auxiliary:
+    output.extend(['serving', 'I-{},'.format(auxiliary)])
+#adds the direction of the highway to the output variable
+output.extend(['going', direction])
 print(' '.join(output))
