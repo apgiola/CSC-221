0b48f8e1b6c9e0d1bef83af14959efda43541b71
apgiola <96624287+apgiola@users.noreply.github.com>
2022-10-26 17:52:32 -0400

    count down added

--- countdowntilmatch.py
+++ countdowntilmatch.py
@@ -3,16 +3,15 @@
 
 # Get integer between 11-100 from user
 num = int(input())
-
 # if value < 11 or > 100
 if num < 11 or num > 100:
-    print('Input must be 11-100')
+    print('Input must be 11-100') #input validation
 else:
-    digits_equal = False
-    while digits_equal == False:
-        print(num)
-        tens_value = (num % 100) // 10
-        ones_value = num % 10
-        if tens_value == ones_value:
-            digits_equal = True
-        num -= 1
+    digits_equal = False #set it 
+    while digits_equal == False: #yep
+        print(num) #print current num
+        tens_value = (num % 100) // 10 #tens
+        ones_value = num % 10 #ones
+        if tens_value == ones_value: 
+            digits_equal = True #end of iteration
+        num -= 1 #decrement num
