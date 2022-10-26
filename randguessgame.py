8d6c02f2d1cbd83f551fbd46a7b9521cf984a18e
apgiola <96624287+apgiola@users.noreply.github.com>
2022-10-26 18:44:29 -0400

    Completed prog, one last change

--- randguessgame.py
+++ randguessgame.py
@@ -11,24 +11,31 @@ rand_num = random.randint(1, 50)
 counter = 0 #counter variable
 usr_num = rand_num % 2 + 3 # pre-define usr_num for while loop, modulo 2 of rand_num + 3 so never outside bounds of loop
 #start the program loop now
+#create too high function
+def tooHigh(): #tooHigh function just cause I can
+    print("Too High!")
+    print()
+def tooLow(): #tooLow function just cause I can 
+    print("Too Low!")
+    print()
+    
 while usr_num > 0 and usr_num < 51: # 0 < input < 51
     usr_num = int(input("Guess a number between 1 and 50, or 0 to quit\n")) #takes user input
     counter += 1 #increment counter
     if usr_num > rand_num and usr_num < 51: # prints higher than random number
-        print("Too High!")
-        print()
+        tooHigh() #print too high
     if usr_num < rand_num and usr_num != 0: # prints lower than random number
-        print("Too Low!")
-        print()
+        tooLow() #print too low
     if usr_num == rand_num: # prints victory message
         print('Congratulations! You have guessed the random number in', counter, 'tries!') #includes number of attempts
         q_answer = input('Do you want to play again? Y/N\n')
-        if q_answer == 'Y' or 'y':
+        if q_answer == 'Y': # prints good luck msg and resets rand_num and usr_num. 
             print('Ok! Good luck!')
             rand_num = random.randint(1, 50)
             usr_num = rand_num % 2 + 3
-        if q_answer == 'N' or 'n':
-            
+            counter = 0 #reset counter
+        if q_answer == 'N': #ends the game for the player.
+            print('Thank you for playing, have a great day!')
             break #end loop
     
 else: # exception handling + quit
