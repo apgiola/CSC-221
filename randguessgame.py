9d649ca4ea8595eeb0ee3457411e12a30be0bddc
apgiola <96624287+apgiola@users.noreply.github.com>
2022-10-26 18:28:50 -0400

    Built replay feature

--- randguessgame.py
+++ randguessgame.py
@@ -5,7 +5,6 @@
 # will ask the user if they want to play again. 
 
 import random
-from urllib.parse import uses_fragment
 #generate a random number between 1 - 50
 rand_num = random.randint(1, 50)
 #counter variable
@@ -13,18 +12,24 @@ counter = 0 #counter variable
 usr_num = rand_num % 2 + 3 # pre-define usr_num for while loop, modulo 2 of rand_num + 3 so never outside bounds of loop
 #start the program loop now
 while usr_num > 0 and usr_num < 51: # 0 < input < 51
-    usr_num = int(input("Input a number between 1 and 50, if you want to give up then type 0")) #takes user input
+    usr_num = int(input("Guess a number between 1 and 50, or 0 to quit\n")) #takes user input
     counter += 1 #increment counter
     if usr_num > rand_num and usr_num < 51: # prints higher than random number
-        print("You have guessed a number higher than the random number")
+        print("Too High!")
         print()
     if usr_num < rand_num and usr_num != 0: # prints lower than random number
-        print("You have guessed a number lower than the random number")
+        print("Too Low!")
         print()
     if usr_num == rand_num: # prints victory message
         print('Congratulations! You have guessed the random number in', counter, 'tries!') #includes number of attempts
-        print()
-        break #end loop
+        q_answer = input('Do you want to play again? Y/N\n')
+        if q_answer == 'Y' or 'y':
+            print('Ok! Good luck!')
+            rand_num = random.randint(1, 50)
+            usr_num = rand_num % 2 + 3
+        if q_answer == 'N' or 'n':
+            
+            break #end loop
     
 else: # exception handling + quit
     if usr_num == 0: # user has quit, receive quit msg
