a5016a6b0d8add768c08dacd7af30c10d432dc71
apgiola <96624287+apgiola@users.noreply.github.com>
2022-10-30 15:03:06 -0400

    Completed, commented. Append list then reverse.

--- upsidedowntri.py
+++ upsidedowntri.py
@@ -1,2 +1,10 @@
+#usr input
 num_input = int(input())
-# input number
+#create blank list
+lst = []
+#create list with all ints between 0 and final usr input
+for i in range(1, num_input + 1):
+    lst.append(i)
+lst.reverse() #reverse list
+for j in lst: #print stars with the reversed list.
+    print('* ' * j)
